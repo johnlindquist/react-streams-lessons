50bfe78b0001fdb63e0c2ebc38ab6ceb5443c03d
John Lindquist <johnlindquist@gmail.com>
2018-08-09 13:47:12 -0600

    hangman

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import React from "react"
 import { render } from "react-dom"
+import { fromEvent, from } from "rxjs"
+import { map, mergeMap, scan, startWith } from "rxjs/operators"
+import { Stream } from "react-streams"
 
-const App = () => <div>Hello world</div>
+const keydown$ = fromEvent(document, "keydown").pipe(
+  map(({ key }) => ({ letter: key }))
+)
+
+const word$ = from("friendly")
+
+const hangman$ = keydown$.pipe(
+  startWith([]),
+  scan((letters, { letter }) => [...letters, letter]),
+  mergeMap(letters =>
+    word$.pipe(
+      map(check => (letters.includes(check) ? check : "*")),
+      scan((prev, next) => `${prev}${next}`)
+    )
+  ),
+  map(result => ({ result }))
+)
+
+const App = () => (
+  <div>
+    <Stream source={keydown$}>{({ letter }) => letter}</Stream>
+    <hr />
+    <Stream source={hangman$}>{({ result }) => result}</Stream>
+  </div>
+)
 
 render(<App />, document.getElementById("root"))
